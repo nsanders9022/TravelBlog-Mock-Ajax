6f339aa4151b7ccec18a946f24d12527de8478d8
Katy Daviscourt and Shruti Priya <katy+shruti@>
2017-04-19 11:22:48 -0700

    add form to update location

--- src/TravelBlog/Controllers/LocationsController.cs
+++ src/TravelBlog/Controllers/LocationsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TravelBlog.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -34,6 +35,20 @@ namespace TravelBlog.Controllers
         public IActionResult Create(Location location)
         {
             db.Locations.Add(location);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisLocation = db.Locations.FirstOrDefault(locations => locations.LocationId == id);
+            return View(thisLocation);
+        }
+
+        [HttpPost]
+        public IActionResult Edit (Location location)
+        {
+            db.Entry(location).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
