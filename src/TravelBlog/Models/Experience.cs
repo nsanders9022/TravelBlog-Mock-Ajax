70b540126af4b0884c4f8542a29e285243c1d18a
Katy Daviscourt and Shruti Priya <katy+shruti@>
2017-04-19 08:45:48 -0700

    add three models, person, location and experience. Link location and experience

---
+++ src/TravelBlog/Models/Experience.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelBlog.Models
+{
+    public class Experience
+    {
+        [Key]
+        public int ExperienceId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int LocationId { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
---
+++ src/TravelBlog/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelBlog.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public int LocationId { get; set; }
+        public int ExperienceId { get; set; }
+    }
+}
