57a888a6e989193e81433a70b382e72027168583
Katy Daviscourt and Shruti Priya <katy+shruti@>
2017-04-19 09:27:04 -0700

    run dotnet ef commands successful to add migration and update database

---
+++ src/TravelBlog/Migrations/20170419162402_Initial.Designer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TravelBlog.Models;
+
+namespace TravelBlog.Migrations
+{
+    [DbContext(typeof(TravelBlogContext))]
+    [Migration("20170419162402_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("TravelBlog.Models.Experience", b =>
+                {
+                    b.Property<int>("ExperienceId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<int>("LocationId");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("ExperienceId");
+
+                    b.HasIndex("LocationId");
+
+                    b.ToTable("Experiences");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.Location", b =>
+                {
+                    b.Property<int>("LocationId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("LocationId");
+
+                    b.ToTable("Locations");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.Experience", b =>
+                {
+                    b.HasOne("TravelBlog.Models.Location", "Location")
+                        .WithMany("Experiences")
+                        .HasForeignKey("LocationId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ src/TravelBlog/Migrations/TravelBlogContextModelSnapshot.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TravelBlog.Models;
+
+namespace TravelBlog.Migrations
+{
+    [DbContext(typeof(TravelBlogContext))]
+    partial class TravelBlogContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("TravelBlog.Models.Experience", b =>
+                {
+                    b.Property<int>("ExperienceId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<int>("LocationId");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("ExperienceId");
+
+                    b.HasIndex("LocationId");
+
+                    b.ToTable("Experiences");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.Location", b =>
+                {
+                    b.Property<int>("LocationId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("LocationId");
+
+                    b.ToTable("Locations");
+                });
+
+            modelBuilder.Entity("TravelBlog.Models.Experience", b =>
+                {
+                    b.HasOne("TravelBlog.Models.Location", "Location")
+                        .WithMany("Experiences")
+                        .HasForeignKey("LocationId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
