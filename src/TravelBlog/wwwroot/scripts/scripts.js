58bc0958a95d506a30d1a147e54c154d6f05612c
Elise St Hilaire, Katy Daviscourt and Nicole Sanders <elise+katy+nicole@>
2017-05-02 13:34:00 -0700

    split first ajax partial view into separate js file

--- src/TravelBlog/wwwroot/scripts/scripts.js
+++ src/TravelBlog/wwwroot/scripts/scripts.js
@@ -1,10 +1,74 @@
 ﻿$(document).ready(function () {
-    $('.hello-ajax').click(function () {
+           $('.hello-ajax').click(function () {
+               $.ajax({
+                   type: 'GET',
+                   dataType: 'html',
+                   url: $(this).data('request-url'),
+                   success: function (result) {
+                       $('#result1').html(result);
+                   }
+               });
+           });
+    $('.sum').click(function () {
         $.ajax({
             type: 'GET',
-            url: '@Url.Action("HelloAjax")',
+            data: { firstNumber: 1, secondNumber: 2 },
+            url: '@Url.Action("Sum")',
             success: function (result) {
-                $('#result1').html(result);
+                $('#result2').html(result);
+            }
+        });
+    });
+    $('.display-object').click(function () {
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json',
+            url: '@Url.Action("DisplayObject")',
+            success: function (result) {
+                var resultString = 'Id: ' + result.id + '<br>City: ' + result.city + '<br>Country: ' + result.country;
+                $('#result3').html(resultString);
+            }
+        });
+    });
+    $('.display-view').click(function () {
+        $.ajax({
+            type: 'GET',
+            dataType: 'html',
+            url: '@Url.Action("DisplayViewWithAjax")',
+            success: function (result) {
+                $('#result4').html(result);
+            }
+        });
+    });
+    $('.display-random-database-items').submit(function (event) {
+        event.preventDefault();
+        console.log($(this).serialize());
+        $.ajax({
+            url: '@Url.Action("RandomDestinationList")',
+            type: 'GET',
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function (result) {
+                var stringResult = '<ul>';
+                for (var i = 0; i < result.length; i++) {
+                    stringResult += '<li>' + result[i].city + ', ' + result[i].country + '</li>';
+                }
+                stringResult += '</ul>';
+                $('#result5').html(stringResult);
+            }
+        });
+    });
+    $('.new-destination').submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            url: '@Url.Action("NewDestination")',
+            type: 'POST',
+            dataType: 'json',
+            data: $(this).serialize(),
+            success: function (result) {
+                var resultMessage = 'You\'ve added a new destination to the database!<br>Id: ' + result.id + '<br>City: ' + result.city + '<br>Country: ' + result.country;
+                $('#result6').html(resultMessage);
             }
         });
     });
